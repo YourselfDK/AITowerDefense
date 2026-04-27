031eff7368d95f53d9cfa1fd081510c75af6f456
Zwahpz <sebastian@opto-reno.dk>
2026-04-27 14:26:25 +0200

    I made an script and path for the enemy to follow

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private Path currentPath;
+    private Vector3 _targetPosition;
+    private int _currentWaypoint;
+    
+    private void Awake()
+    {
+        currentPath = GameObject.Find("Path1").GetComponent<Path>();
+    }
+    private void OnEnable()
+    {
+        _currentWaypoint = 0;
+        _targetPosition = currentPath.GetPosition(_currentWaypoint);   
+    }
+    void Update()
+    {
+    transform.position = Vector3.MoveTowards(transform.position, _targetPosition, moveSpeed * Time.deltaTime);
+    float relativeDistance = (transform.position - _targetPosition).magnitude;
+    if(relativeDistance < 0.1f)
+        {
+            if(_currentWaypoint < currentPath.Waypoints.Length - 1)
+            {
+            _currentWaypoint++;
+            _targetPosition = currentPath.GetPosition(_currentWaypoint);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
