b5fa87f04268d95fbedbcb32b8b3661efc71ced5
YourselfDK <johan.bergmann@hotmail.com>
2026-04-27 15:47:31 +0200

    Added snippet randomizer

--- Assets/Scripts/IntelSnippetRandomizer.cs
+++ Assets/Scripts/IntelSnippetRandomizer.cs
@@ -8,16 +8,26 @@ public class IntelSnippetRandomizer : MonoBehaviour
 {
 
     public TMP_Text myTextField;
+    [SerializeField] bool trueInformation = true;
+    [SerializeField] int maxAmountOfSnippets = 5;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnEnable()
     {
         //This needs to pick a random number between 1-3 (max = level reached or something)
         //Then it needs to pick a number of random rules = random number.
         //Then it needs to pick 1 random snippet from each rule chosen.
         //This code should be attached to a prefab that can reference a TMPro textbox attached to itself.
         //Possibly place the strings in a scriptable object.
-        PickRandomFromTrueList(5);
+        int amountOfSnippets = UnityEngine.Random.Range(1, maxAmountOfSnippets+1);
+        if(trueInformation == true)
+        {
+            PickRandomFromTrueList(amountOfSnippets);
+        }
+        if(trueInformation == false)
+        {
+            PickRandomFromFalseList(amountOfSnippets);
+        }
     }
 
     // Update is called once per frame
@@ -41,7 +51,7 @@ public class IntelSnippetRandomizer : MonoBehaviour
         }
     }
 
-    public void PickRandomFromFalseList(int falseSnippets, int trueSnippets)
+    public void PickRandomFromFalseList(int snippets)
     {
         string[] mainTextFalse = new string[]
         {
@@ -49,11 +59,14 @@ public class IntelSnippetRandomizer : MonoBehaviour
             "RonFalse ",
             "HermioneFalse "
         };
+        int falseSnippets = UnityEngine.Random.Range(1, snippets+1);
         for (int x = 0; x < falseSnippets; x++)
         {
             string randomFalseIntel = mainTextFalse[UnityEngine.Random.Range(0, mainTextFalse.Length)];
             myTextField.text = myTextField.text + randomFalseIntel;
         }
-        PickRandomFromTrueList(trueSnippets);
+        PickRandomFromTrueList(snippets-falseSnippets);
     }
+
+
 }
