2560b4191e576739780d0357c9e98b05d9774a0b
GS-KIBB <71324003+Korny50@users.noreply.github.com>
2026-04-28 16:32:35 +0200

    BookWorksNow

--- Assets/Scripts/BookButtons.cs
+++ Assets/Scripts/BookButtons.cs
@@ -7,6 +7,7 @@ public class BookController : MonoBehaviour
     public Button right;
     public Button left;
     public Image book;
+    public GameObject myCanvas;
 
     public Sprite[] pages;
     public GameObject[] pageText;
@@ -41,6 +42,9 @@ public class BookController : MonoBehaviour
             currentPage--;
             audioSource.PlayOneShot(pageTurnClip);
             UpdateBook();
+        } else
+        {
+            myCanvas.SetActive(false);
         }
     }
 
@@ -53,7 +57,7 @@ public class BookController : MonoBehaviour
             pageText[i].SetActive(i == currentPage);
         }
 
-        left.interactable = currentPage > 0;
+        left.interactable = currentPage >= 0;
         right.interactable = currentPage < pages.Length - 1;
     }
 }
