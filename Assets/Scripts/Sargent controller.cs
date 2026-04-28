80b4cae77021464381dc68ee3dfccfbb49750564
DortheaGaming <ditteringo@gmail.com>
2026-04-28 21:23:28 +0200

    finished sargent

--- Assets/Scripts/Sargent controller.cs
+++ Assets/Scripts/Sargent controller.cs
@@ -9,6 +9,11 @@ public class Sargentcontroller : MonoBehaviour
     public GameObject speechThingy; 
     public TMPro.TextMeshProUGUI speechText;
     public AudioSource audioSource;
+    public AudioClip sigh;
+    public AudioClip clearsthroat;
+    public AudioClip grunt;
+    public AudioClip growl;
+
    void Start()
     {
         backToGame.onClick.AddListener(ToGame);
@@ -29,30 +34,35 @@ public class Sargentcontroller : MonoBehaviour
     public void endOne()
     {
         Sargent.SetActive(true);
+        audioSource.PlayOneShot(grunt);
         ShowSpeech("You even understand what’s going on yet?", 56f, 218f);
     }
 
     public void endTwo()
     {
         Sargent.SetActive(true);
+        audioSource.PlayOneShot(clearsthroat);
         ShowSpeech("Hmmm, but even a child could have caught that.", 56f, 218f);
     }
 
     public void endThree()
     {
         Sargent.SetActive(true);
+        audioSource.PlayOneShot(sigh);
         ShowSpeech("I guess I’ll leave you to your work.", 56f, 218f);
     }
 
     public void endFive()
     {
         Sargent.SetActive(true);
+        audioSource.PlayOneShot(growl);
         ShowSpeech("01001101 01100001 01111001 01100010 01100101 00100000 01111001 01101111 01110101 00100111 01110010 01100101 00100000 01101110 01101111 01110100 00100000 01101000 01100001 01101100 01100110 00100000 01100010 01100001 01100100", 56f, 808f);
     }
 
     public void endSeven()
     {
         Sargent.SetActive(true);
+        audioSource.PlayOneShot(grunt);
         ShowSpeech("This must be the last wave!", 56f, 218f);
     }
 }
