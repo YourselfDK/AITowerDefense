b9623117ccaca6842ca8e10cf7f2ee12ee61d152
GS-KIBB <71324003+Korny50@users.noreply.github.com>
2026-04-28 19:14:13 +0200

    WinAndFailureImplemented

---
+++ Assets/Scripts/SuccessState.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SuccessState : MonoBehaviour
+{
+    private int friendliesKilled;
+    private int wavesDone;
+    public TextMeshProUGUI kills;
+    public TextMeshProUGUI waves;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        friendliesKilled = GameManager.currentLivesByMistake;
+        wavesDone = WaveController.currentWave; 
+        
+        kills.text = friendliesKilled.ToString();
+        waves.text = wavesDone.ToString();
+    }
+    public void retry()
+    {
+        SceneManager.LoadScene(1);
+        GameManager.currentLivesByMistake = 0;
+        WaveController.currentWave = 1;
+        GameManager.currentLives = 10;
+    }
+}
