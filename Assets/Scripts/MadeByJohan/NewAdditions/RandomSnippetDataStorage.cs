7c9831eeb6d389ec261409c2ba8c2ff5d23b062b
YourselfDK <johan.bergmann@hotmail.com>
2026-05-14 12:33:43 +0200

    Organised files into a folder, added scriptableObject

---
+++ Assets/Scripts/MadeByJohan/NewAdditions/RandomSnippetDataStorage.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RandomSnippetDataStorage", menuName = "Scriptable Objects/RandomSnippetDataStorage")]
+public class RandomSnippetDataStorage : ScriptableObject
+{
+    
+}
