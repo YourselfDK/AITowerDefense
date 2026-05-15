532e74015fdd10343bbeba91f2004939044dbea1
Johan Bergmann <johan.bergmann@hotmail.com>
2026-05-15 03:33:16 +0200

    Tried adding a scriptable object
    containing the strings used in SnippetRandomizer.

--- Assets/Scripts/MadeByJohan/NewAdditions/RandomSnippetDataStorage.cs
+++ Assets/Scripts/MadeByJohan/NewAdditions/RandomSnippetDataStorage.cs
@@ -1,7 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "RandomSnippetDataStorage", menuName = "Scriptable Objects/RandomSnippetDataStorage")]
 public class RandomSnippetDataStorage : ScriptableObject
 {
-    
+    public List<string> mainTextTrue = new List<string>
+        {
+            "This data seems to be owned and controlled by the US millitary.\n",
+            "This intel was processed to... 'beat the bad guys'?\n",
+            "'TOP SECRET INTEL, eyes off!'\n",
+            "They stored this file since 1978.\n",
+            "This paper contains a complaint lodged to Sergeant Chatt.\n",
+            "*A qoute from a civilian.*\n",
+            "AI prompt: How do I code secure communications - God save America!\n",
+            "AI prompt: Give me a recipe for chocolate cupcakes - God save America!\n",
+            "AI answer: Guns, burgers and FREEDOM!\n",
+            "AI answer: Use ALL-AMERICAN CAKEMIX in cupcake cups!\n",
+            "AI answer: Make sure NO enemies of the GREAT US of A can crack it!\n",
+            "Generated with Eag.LE.\n",
+            "AI version: 1.18.\n",
+            "It seems they trained this AI on 02/20/2020.\n",
+            "AI trained on 02/20/2021.\n",
+            "This document violates... every part of GDPR.\n",
+            "I've never seen this many violations of GDPR.\n",
+            "List of recipes, including Major Tom's favorite cupcake recipe. A letter of his consent is included.\n",
+            "[DATA EXPUNGED] *there is a mouse-shaped hole in this intel.*\n",
+            "[DATA EXPUNGED]ney plus. Includes a note 'REDACT THIS IMMEDIATELY!'.\n",
+            "George Washington was the first Commanding General of the US army.\n",
+            "AI platform evaluation: An AI provided recipe, and seven different cooking blogs about the same food.\n",
+            "AI answer: David Bowie IS currently alive, wish he was American!\n",
+            "Unequivocal consent means it is NOT implied.\n",
+            "Informed consent means the subject knows their rights.\n",
+            "Specific consent is only for one particular assignment or purpose.\n",
+            "Voluntary consent means there is NOT any disadvantages in declining to consent.\n"
+        };
 }
